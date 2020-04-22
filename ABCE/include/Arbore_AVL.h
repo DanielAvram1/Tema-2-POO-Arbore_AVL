2d6abc343ed557ebed45f1f9bd91d7847a448b64
DanielAvram1 <62517917+DanielAvram1@users.noreply.github.com>
2020-04-22 18:56:09 +0300

    Add files via upload

---
+++ ABCE/include/Arbore_AVL.h
@@ -0,0 +1,27 @@
+#ifndef ARBORE_AVL_H
+#define ARBORE_AVL_H
+#include "Arbore.h"
+#include "Nod_AVL.h"
+#include <iostream>
+using namespace std;
+
+class Arbore_AVL: public Arbore
+{
+private:
+    Nod_AVL* rad;
+public:
+    Arbore_AVL();
+    Arbore_AVL(Nod_AVL*);
+    Arbore_AVL(const Arbore_AVL&);
+    ~Arbore_AVL();
+    friend ostream &operator <<(ostream &out, Arbore_AVL &arb);
+    friend istream &operator>>(istream &in, Arbore_AVL &arb);
+    void afisareEchilibru();
+    virtual void insert(int);
+    virtual void erase(int);
+    virtual bool find(int);
+
+
+};
+
+#endif // ARBORE_AVL_H
